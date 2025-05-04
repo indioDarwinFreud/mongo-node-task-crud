e5cf1d2744137a1c3a7d98fe2f336d23a43a81d0
Luis Lovos <luisestebanlovos@gmail.com>
2025-05-04 20:27:15 -0300

    Update

---
+++ src/uploads/multer.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Carpeta fuera de src
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname); // Nombre único para cada archivo
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
