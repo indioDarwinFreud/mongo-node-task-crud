df0156cfc708ba9bb5ef3f7c70ad0a7c995649a3
Luis Lovos <luisestebanlovos@gmail.com>
2025-05-04 21:45:17 -0300

    Update

--- src/database.js
+++ src/database.js
@@ -3,7 +3,12 @@ import { MONGODB_URI } from './config.js';
 
 (async () => {
   try {
-    const db = await connect(MONGODB_URI);
+    const db = await connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 30000, // 30 segundos
+      serverSelectionTimeoutMS: 30000, // 30 segundos
+    });
     console.log("DB connected to", db.connection.name);
   } catch (error) {
     console.error("❌ Error al conectar con MongoDB:", error);
