4d444ecaa3f813db01b576c26ab33c8ea2464253
Luis Lovos <luisestebanlovos@gmail.com>
2025-08-15 14:56:41 -0300

    Update

--- src/uploads/multer.js
+++ src/uploads/multer.js
@@ -1,21 +1,15 @@
+// src/uploads/multer.js
 import multer from "multer";
-import path from "path";
-import { fileURLToPath } from "url"; // Importa la función para obtener la ruta del archivo
-
-// Convierte la URL del archivo a una ruta de archivo
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // Usa la ruta `__dirname` que acabas de definir
-    cb(null, path.join(__dirname, '..', '..', 'uploads'));
+    cb(null, "uploads/"); // Carpeta fuera de src
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname); // Nombre único para cada archivo
   },
 });
 
 const upload = multer({ storage });
 
-export default upload;
+export default upload; // Exportación predeterminada
