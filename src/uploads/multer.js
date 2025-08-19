c06af1a84a5f156966c167b30b29e5730cee862e
Luis Lovos <luisestebanlovos@gmail.com>
2025-08-19 20:36:06 -0300

    Update

--- src/uploads/multer.js
+++ src/uploads/multer.js
@@ -1,9 +1,9 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-   
-    cb(null, "./uploads/");
+    cb(null, path.resolve('uploads'));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
