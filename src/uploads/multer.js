b8306960cd36e49885691c1f46a2cbd02e58532f
Luis Lovos <luisestebanlovos@gmail.com>
2025-08-19 21:09:34 -0300

    Update

--- src/uploads/multer.js
+++ src/uploads/multer.js
@@ -1,9 +1,10 @@
-import path from "path";
 import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.resolve('uploads'));
+    // La carpeta 'uploads' está al mismo nivel que este archivo multer.js
+    // Esto es lo que causó el error ENOENT: se busca la carpeta en un lugar diferente
+    cb(null, "./");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
